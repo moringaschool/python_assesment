ef7c93d65dd90a800f0438cb19006da461ff69e7
mussaimo <okemwamoses@gmail.com>
2016-08-03 19:27:48 +0300

    x and o

---
+++ xoxo/xoxo.py
@@ -0,0 +1,18 @@
+# Using Python, have the function xoxo(str) take the str parameter
+# being passed and return the string true if there is an equal number of x's and o's,
+# because you cannot recieve an uneven number of hugs and kisses :-)
+# otherwise return the string false. Only these two letters will be entered in the string,
+# no punctuation or numbers.
+# For example: if str is "xooxxxxooxo" then the output should return false because there are 6 x's and 5 o's.
+
+
+def Xoxo(str):
+    # code goes here
+
+
+
+
+
+
+# keep the function call
+print Xoxo("have fun..xoxo")
