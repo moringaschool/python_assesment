908c76233073dc6b448c8a51ef9b885aab3daec6
orchardsonbett <orchardsonbett@gmail.com>
2016-08-05 17:46:14 +0300

    solution

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -5,4 +5,30 @@
 # Not balanced:
 #   '((()'
 #   '())('
+#
+#
 
+
+def parchecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
