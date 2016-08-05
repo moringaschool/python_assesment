88f7888b1c2f311a543faffaa907b07881cf4a8d
orchardsonbett <orchardsonbett@gmail.com>
2016-08-05 17:45:57 +0300

    solution

--- xoxo/xoxo.py
+++ xoxo/xoxo.py
@@ -8,11 +8,11 @@
 
 def Xoxo(str):
     # code goes here
-
-
-
-
-
+    string = string.lower()
+    if string.find("x") != -1 or string.find("o") != -1:
+        return string.count("x") == string.count("o")
+    else:
+        return False
 
 # keep the function call
-print Xoxo("have fun..xoxo")
+print (Xoxo("have fun..xoxo"))
