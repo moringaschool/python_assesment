d3ec92689ee9995c0a1d46c49e0300aabeb59a93
Mark Mwaura <markkmwaura@gmail.com>
2016-08-05 16:47:55 +0300

    Revert "paren match"

--- solutions/question_three.py
+++
@@ -1,8 +0,0 @@
-def sequence():
-    items = []
-    num = [x for x in raw_input().split(',')]
-    for p in num:
-        x = int(p, 2)
-        if not x%5:
-            items.append(p)
-    print(','.join(items))
